b090414ff6879fe93000d50a694d9b8b2d6a3a04
Cindy Salinas <cindysalinas15@gmail.com>
2016-09-12 15:00:04 -0400

    init app

---
+++ client/app.modules.js
@@ -0,0 +1,6 @@
+
+'use strict';
+
+angular.module('fullstackApp', [
+    'ui.router'
+  ]);
---
+++ client/app.routes.js
@@ -0,0 +1,23 @@
+
+'use strict';
+/**
+ * Load states for application (UI-Router)
+ */
+angular.module('fullstackApp')
+    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider){
+
+    // any unknown URLS go to 404
+    $urlRouterProvider.otherwise('/404');
+    // no route goes to index
+    $urlRouterProvider.when('', '/');
+    // use a state provider for routing
+    $stateProvider
+      .state('app', {
+        abstract: true,
+        templateUrl: "./views/home/homeView.html",
+      })
+      .state('404', {
+          url: '/404',
+          templateUrl: './views/not_found/404.html'
+      })
+}]);
