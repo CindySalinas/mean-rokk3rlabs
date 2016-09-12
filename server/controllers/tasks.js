836eace37e716219cdade49c90645740b3269d7a
Cindy Salinas <cindysalinas15@gmail.com>
2016-09-12 17:10:41 -0400

    update task

--- server/controllers/tasks.js
+++ server/controllers/tasks.js
@@ -159,6 +159,96 @@ router.get('/destroy/:id', function(req, res) {
 				});
 	});
 });
+/**
+  * /task/update/:id (POST)
+
+  * Update task
+
+  * @params
+    * id(mongoId)
+    * name(string)
+    * dueDate(date(YYYY-MM-DD))
+    * priority(int(1..5))
+
+   * return Object
+*/
+router.post('/update/:id', function(req, res) {
+	//validate params
+	req.checkParams({
+		id: {
+			notEmpty: {
+				errorMessage: 'Id is required'
+			},
+			isMongoId: {
+				errorMessage: 'Invalid id field'
+			}
+		}
+	});
+	req.checkBody({
+		name: {
+			notEmpty: {
+				errorMessage: 'Name is required'
+			},
+			isString: {
+				errorMessage: 'Invalid name field'
+			}
+		},
+		dueDate:{
+			notEmpty: {
+				errorMessage: 'dueDate is required'
+			},
+			isValidDate: {
+				options: ['YYYY-MM-DD'],
+				errorMessage: 'Invalid date'
+			}
+		},
+		priority:{
+			notEmpty: {
+				errorMessage: 'priority is required'
+			},
+			isInt: {
+				options:[{
+          min: 1,
+          max: 5
+        }],
+        errorMessage: 'priority field must be between 1 and 5'
+			}
+		}
+	});
+	//validate error
+	var errors = req.validationErrors();
+	//send error
+	if(errors)
+		return res
+			.status(500)
+			.send({
+				status: 500,
+				errors: errors
+			});
+	//update
+	Task.findOneAndUpdate({
+		_id: req.params.id
+	}, {
+		name: req.body.name,
+		priority: req.body.priority,
+		due_date: req.body.dueDate
+	},{
+		new: true
+	}, function(err, task) {
+		if (err){
+			return res
+				.status(500)
+				.send({
+					status: 500,
+					errors: err
+				});
+		}
+		task = task.toObject();
+		task.overdue = overdue(task.due_date);
+		return res.status(200).send(task);
+	});
+});
+
 function overdue(date){
 	var now = moment().format('YYYY-MM-DD');
 	if(moment(date, 'YYYY-MM-DD').diff(now, 'day') > 0)
